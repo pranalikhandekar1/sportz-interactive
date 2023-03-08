db754f3e54f785a50e857d4e81915947a40674fe
Ratnadip K <pranalikhandekar1@gmail.com>
2023-03-08 15:18:39 +0530

    code pushed1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Player } from "./components/player";
-import { Position } from "./components/position";
 
 function App() {
   return (
     <div className="App">
-      {/* <Player /> */}
-      <Position />
+      <Player />
     </div>
   );
 }
